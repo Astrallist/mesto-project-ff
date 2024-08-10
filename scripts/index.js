cb636cb0329bdd6c38821597e7819ecb6c1e8932
Astrallist <132389598+Astrallist@users.noreply.github.com>
2024-08-10 12:31:13 +0300

    10.08.24

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function createCard(name, link, removeCard) {
-    const cardContent = cardTemplate.querySelector('.card').cloneNode(true);
-    cardContent.querySelector('.card__title').textContent = name;
-    cardContent.querySelector('.card__image').src = link;
-    const deleteButton = cardContent.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', removeCard);
-    return cardContent;
-  };
+  const cardContent = cardTemplate.querySelector(".card").cloneNode(true);
+  cardContent.querySelector(".card__title").textContent = name;
+  cardContent.querySelector(".card__image").src = link;
+  const deleteButton = cardContent.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", removeCard);
+  return cardContent;
+}
 
 // @todo: Функция удаления карточки
 function removeCard(evt) {
-    const evtTarget = evt.target;
-    const card = evtTarget.closest('.card');
-    card.remove();
-  };
+  const evtTarget = evt.target;
+  const card = evtTarget.closest(".card");
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
-  initialCards.forEach(function (card) { 
-    const cardContent = createCard(card.name, card.link, removeCard);
-    placesList.append(cardContent);
-  });
+initialCards.forEach(function (card) {
+  const cardContent = createCard(card.name, card.link, removeCard);
+  placesList.append(cardContent);
+});
