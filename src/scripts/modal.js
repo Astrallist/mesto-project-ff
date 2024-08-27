532b5b020a03713a6146ee09c2cbe55f6a5d766c
Astrallist <132389598+Astrallist@users.noreply.github.com>
2024-08-27 22:43:08 +0300

    Update modal.js

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -5,15 +5,19 @@ export function openModal(popup) {
 
 export function closeModal(popup) {
   popup.classList.remove("popup_is-opened");
-  document.addEventListener("keydown", closeByEscape);
+  document.removeEventListener("keydown", closeByEscape);
 }
 
 export function closeByEscape(evt) {
-  const popup = document.querySelector(".popup_is-opened");
-  if (evt.key === "Escape") closeModal(popup);
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_is-opened");
+    closeModal(popup);
+  }
 }
 
 export function closeByOverlay(evt) {
-  const popup = document.querySelector(".popup_is-opened");
-  if (evt.target.matches(".popup_is-opened, .popup__close")) closeModal(popup);
+  if (evt.target.matches(".popup_is-opened, .popup__close")) {
+    const popup = document.querySelector(".popup_is-opened");
+    closeModal(popup);
+  }
 }
