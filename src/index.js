bb2ab1bc7f5d0fd9ade0da11deb5baef84ff440b
Astrallist <132389598+Astrallist@users.noreply.github.com>
2024-09-22 15:22:25 +0300

    1 попытка

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
+console.log('Версия 50');
+
 import "./pages/index.css";
-import { initialCards } from "./scripts/cards";
+//import { initialCards } from "./scripts/cards";  НЕ АКТУАЛЬНО
 import { createCard, likeCard, removeCard } from "./scripts/card";
 import { openModal, closeModal, closeByOverlay } from "./scripts/modal";
+import { enableValidation, clearValidation } from "./scripts/validation";
+
+// Валидация
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+import { getInitialCards, getProfileInfo, editProfileInfo, addNewCard, editProfileAvatar } from './scripts/api.js'
+
+const titleProfile = document.querySelector(".profile__title");
+let profileId = null;
+let newCardThis = false;
+
+const popupAvatar = document.querySelector('.popup_type_edit-avatar'); //окно смены аватара
+const formAvatar = document.querySelector('form[name="edit-avatar"]'); //форма ввода данных для аватара
+const avatarInput = formAvatar.querySelector('.popup__input_type_avatar'); //ввод адреса на новый аватар
+const avatarProfile = document.querySelector('.profile__image'); //аватар на странице
+
+//выводим информацию в профиле
+getProfileInfo()
+  .then((result) => {
+    const jobInputValue = result.about;
+    const nameInputValue = result.name;
+
+    const descriptionProfile = document.querySelector(".profile__description");
+
+    avatarProfile.setAttribute(
+      "style",
+      `background-image: url('${result.avatar}')`
+    );
+    titleProfile.textContent = nameInputValue;
+    descriptionProfile.textContent = jobInputValue;
+
+    profileId = result._id;
+
+  })
+  .catch((err) => {
+    console.log(err); // выводим ошибку в консоль
+  });
+
+//выводим все карточки группы
+getInitialCards()
+  .then((result) => {
+    // обрабатываем результат
+    result.forEach(function (card) {
+      const cardContent = createCard(card, profileId, newCardThis, likeCard, removeCard, openCard);
+      placesList.append(cardContent);
+    });
+  })
+  .catch((err) => {
+    console.log(err); // выводим ошибку в консоль
+  });
+
 
 // @todo: DOM узлы
 const placesList = document.querySelector(".places__list");
 
 //Открытие и закрытие модальных окон
+
 const popup = document.querySelectorAll(".popup");
+
 //Окно редактирования
 const buttonEditProfile = document.querySelector(".profile__edit-button");
 const popupEditProfile = document.querySelector(".popup_type_edit");
 buttonEditProfile.addEventListener("click", function () {
+  clearValidation(formProfile, validationConfig);
   openModal(popupEditProfile);
   fillPopupEditInputs();
 });
@@ -22,7 +85,6 @@ const popupAddProfile = document.querySelector(".popup_type_new-card");
 buttonAddProfile.addEventListener("click", () => openModal(popupAddProfile));
 
 //Функция открытия карточки
-
 export function openCard(card) {
   const popupImage = document.querySelector(".popup_type_image");
 
@@ -41,35 +103,33 @@ document.addEventListener("click", (evt) => {
 
 popup.forEach((popup) => popup.addEventListener("mousedown", closeByOverlay));
 
-// @todo: Вывести карточки на страницу
+/*
+// @todo: Вывести карточки на страницу НЕ АКТУАЛЬНО
 initialCards.forEach(function (card) {
   const cardContent = createCard(card, likeCard, removeCard, openCard);
   placesList.append(cardContent);
 });
+*/
 
 //Изменение данных профиля
-// Находим форму в DOM
-const formProfile = document.querySelector('.popup__form[name="edit-profile"]'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-const nameInput = formProfile.querySelector('input[name="name"]'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formProfile.querySelector('input[name="description"]'); // Воспользуйтесь инструментом .querySelector()
+const formProfile = document.querySelector('.popup__form[name="edit-profile"]');
+const nameInput = formProfile.querySelector('input[name="name"]');
+const jobInput = formProfile.querySelector('input[name="description"]');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function chanceFormProfile(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+  evt.preventDefault();
+  loading(true, formPlace);//начало загрузки
 
-  // Получите значение полей jobInput и nameInput из свойства value
   const jobInputtValue = jobInput.value;
   const nameInputValue = nameInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
   const titleProfile = document.querySelector(".profile__title");
   const descriptionProfile = document.querySelector(".profile__description");
-  // Вставьте новые значения с помощью textContent
+
   titleProfile.textContent = nameInputValue;
   descriptionProfile.textContent = jobInputtValue;
+
+  editProfileInfo(nameInputValue, jobInputtValue);//меняем данные на сервере
+  loading(false, formPlace);//конец загрузки
   closeModal(popupEditProfile);
 }
 
@@ -81,27 +141,76 @@ function fillPopupEditInputs() {
   jobInput.value = descriptionProfile.textContent;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formProfile.addEventListener("submit", chanceFormProfile);
 
 //Добавление новой карточки
-// Находим форму в DOM
 const formPlace = document.querySelector('.popup__form[name="new-place"]');
-// Находим поля формы в DOM
 const placeInput = formPlace.querySelector('input[name="place-name"]');
 const linkInput = formPlace.querySelector('input[name="link"]');
 
+const buttonNewCard = document.querySelector('#button_popup_type_new-card');
+
 function createNewCard(evt) {
   evt.preventDefault();
+  loading(true, formPlace);//начало загрузки
   const card = new Object();
   card.name = placeInput.value;
   card.link = linkInput.value;
 
-  const cardContent = createCard(card, likeCard, removeCard, openCard);
+  buttonNewCard.setAttribute("disabled", true);
+  buttonNewCard.classList.add(validationConfig.inactiveButtonClass);
+  clearValidation(formPlace, validationConfig); //очищаем поле
+
+  newCardThis = true;
+  const cardContent = createCard(card, profileId, newCardThis, likeCard, removeCard, openCard);
   placesList.prepend(cardContent);
   formPlace.reset();
+  loading(false, formPlace);//конец загрузки
+  newCardThis = false;
+
+  addNewCard(card);//сохранение карточки на сервере
   closeModal(popupAddProfile);
 }
 
+//Функция обновления аватар
+function chanceAvatarProfile(evt) {
+  evt.preventDefault();
+  loading(true, formPlace);//начало загрузки
+
+  const avatarUrl = `url(${avatarInput.value})`;
+  const avatar = avatarInput.value;
+  avatarProfile.style.backgroundImage = avatarUrl;
+
+  editProfileAvatar(avatar);
+  loading(false, formPlace);//конец загрузки
+  closeModal(popupAvatar);
+};
+
+//кнопка и аватарка, смена аватарки
+avatarProfile.addEventListener('click', function (evt) {
+  clearValidation(formAvatar, validationConfig);
+  formAvatar.reset();
+  openModal(popupAvatar);
+});
+
+//Сменить аватар
+formAvatar.addEventListener('submit', function (evt) {
+  chanceAvatarProfile(evt)
+});
+
+//Загрузка
+function loading(status, popup) {
+  const submitFormEnd = popup.querySelector('.popup__button')
+  if (status) {
+    submitFormEnd.textContent = 'Сохранение...'
+  } else {
+    submitFormEnd.textContent = 'Сохранить'
+  };
+};
+
 formPlace.addEventListener("submit", createNewCard);
+
+//Настройки валидации
+enableValidation(validationConfig);
+buttonNewCard.setAttribute("disabled", true);
+buttonNewCard.classList.add(validationConfig.inactiveButtonClass);
